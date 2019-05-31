506b0d9b5accac8dd2f4e2f384369876f5f8acc2
AN K <a2nkale@gmail.com>
2019-05-31 16:17:36 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -13,5 +13,5 @@ def reverse_each_word(string)
   array.collect do|string| #iterate over the array
     new_array << string.reverse #reverse each word in the array
   end
-  test_array.join(" ")
+  new_array.join(" ")
 end
